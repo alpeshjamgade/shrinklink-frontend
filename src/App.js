442930cd6a565f6d6c2dee456cf321c275fc708c
Alpesh Jamgade <alpesh@tradelab.in>
2024-09-21 00:13:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    setLoading(true);
+    setResponseMessage("");
+    setInputValue("");
+
+    const payload = {
+      url: inputValue,
+    };
+
+    try {
+      var response = await fetch("http://localhost:9999/api/urls", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (!response.ok) {
+        throw new Error(response);
+      }
+
+      response = await response.json();
+      setResponseMessage(`Short Url : ${response.data.short_url}`);
+    } catch (error) {
+      console.error("Error posting data:", error.message);
+      setResponseMessage("Invalid response from backend");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1> Welcome to my app </h1>
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Enter your url"
+          />
+          <button onClick={handleButtonClick} disabled={loading}>
+            {loading ? "Loading..." : "Submit"}
+          </button>
+
+          {responseMessage && <p>{responseMessage}</p>}
+        </div>
       </header>
     </div>
   );
